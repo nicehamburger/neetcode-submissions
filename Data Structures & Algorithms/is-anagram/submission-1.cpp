524f5ddebf4869e49c3e50951a24f7b9c827629a
Hamdan Raashid <145169024+nicehamburger@users.noreply.github.com>
2026-05-05 14:15:57 -0600

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.cpp
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int s_len = s.length();
-        int t_len = t.length();
 
-        if (s_len != t_len) {
+        // Check size equal
+        if (s.length() != t.length()) {
             return false;
         }
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // Sort Characters
+        std::sort(s.begin(),s.end());
+        std::sort(t.begin(),t.end());
 
-        return s == t;
-
-
-        // for(int i = 0; i < s_len; i++) {
-        //     if (s[i] != t[i]) {
-        //         return false;
-        //     }
-        // }
-        // return true
+        // Compare
+        return (s == t);
     }
 };
 
-// check equal length ==> if not -- return false
-// sort the letters in s and t
-// iterate through each of the strings - even if at one iteration
-// its not same --- return false; else continue & return true in the end
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
