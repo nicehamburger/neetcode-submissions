260e3b0ec58385819f60399b174358dd868f7164
Hamdan Raashid <145169024+nicehamburger@users.noreply.github.com>
2026-05-05 14:25:17 -0600

    Update: is-anagram - submission-3

--- Data Structures & Algorithms/is-anagram/submission-3.cpp
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
+        // Check size equal
         if (s.length() != t.length()) {
             return false;
         }
-
-        int freq_count[26] = {0};
+        
+        // Build frequency array
+        int freq[26] = {0};
 
         for (int i = 0; i < s.length(); i++) {
-            freq_count[s[i] - 'a']++;
-            freq_count[t[i] - 'a']--;
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
-        
-        for (int val: freq_count) {
-            if (val != 0) return false;
+
+        for (int i: freq) {
+            if (i != 0) return false;
         }
         return true;
     }
 };
+
+// Given s and t only have lowercase letters
+// Assume 'a' is at index 0
+// Therefore, 'a' - 'a' = 0
+// Character's index found when subtracted by 'a'
